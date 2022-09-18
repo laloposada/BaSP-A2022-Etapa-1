3edc913d4cf06a51f3aa2888e8020044159a120d
Gonzalo Posada <gposipha@gmail.com>
2022-09-18 11:51:48 -0300

    BaSP-W05-2022: made week 5 folder and did block 1 of exercises

---
+++ Week-05/exercises/01_variables_and_operators.js
@@ -0,0 +1,22 @@
+console.log("Exercise 01: Variables and Operators.");
+
+//Exercise 01-a
+console.log("Exercise 01-a:");
+var numberOne = 10;
+var numberTwo = 20;
+var addition = numberOne + numberTwo;
+console.log("Result:" + addition);
+
+//Exercise 01-b
+console.log("Exercise 02-b:");
+var city = "London,";
+var country = "England";
+var nationality = city + " " + country;
+console.log("She was born in" + " " + nationality);
+
+//Exercise 01-c
+console.log("Exercise 02-c:");
+var nameMom = "Jessy";
+var nameDad = "Tom";
+var largeName = nameMom.length + nameDad.length;
+console.log("Result: " + largeName);
