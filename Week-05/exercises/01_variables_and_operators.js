36830c78ee8447c5a35a3d5ba269992b63992c43
Gonzalo Posada <gposipha@gmail.com>
2022-09-18 13:35:07 -0300

    BaSP-w05-2022: completed exercises of the strings block

--- Week-05/exercises/01_variables_and_operators.js
+++ Week-05/exercises/01_variables_and_operators.js
@@ -8,14 +8,14 @@ var addition = numberOne + numberTwo;
 console.log("Result:" + addition);
 
 //Exercise 01-b
-console.log("Exercise 02-b:");
+console.log("Exercise 01-b:");
 var city = "London,";
 var country = "England";
 var nationality = city + " " + country;
 console.log("She was born in" + " " + nationality);
 
 //Exercise 01-c
-console.log("Exercise 02-c:");
+console.log("Exercise 01-c:");
 var nameMom = "Jessy";
 var nameDad = "Tom";
 var largeName = nameMom.length + nameDad.length;
--- Week-05/exercises/02_strings.js
+++ Week-05/exercises/02_strings.js
@@ -1,1 +1,37 @@
 console.log("Exercise 02: Strings.");
+
+//Exercise 02-a
+console.log("Exercise 02-a:");
+var text = "Hello World!";
+var textUp = text.toUpperCase();
+console.log(textUp);
+
+//Exercise 02-b
+console.log("Exercise 02-b:");
+var textOne = "Hello Uruguay!";
+var subText = textOne.substring(0, 5);
+console.log(subText);
+
+//Exercise 02-c
+console.log("Exercise 02-c:");
+var textTwo = "Hello Argentina!";
+var textSub = textTwo.substring(13);
+console.log(textSub);
+
+//Exercise 02-d
+console.log("Exercise 02-d:");
+var textThree = "we are Radium Rocket!";
+var textCapi = textThree.substring(0, 1).toUpperCase() + textThree.substring(1, textThree.length).toLowerCase();
+console.log(textCapi);
+
+//Exercise 02-e
+console.log("Exercise 02-e:");
+var textFour = "My name is Gonzalo.";
+var blankSpace = textFour.indexOf(" ", 0);
+console.log("First blank space: " + blankSpace);
+
+//Exercise 02-f
+console.log("Exercise 02-f:");
+var textFive = "bootCamp-2022 by-radium-rocket";
+var textCapitalized = textFive.substring(0, 1).toUpperCase() + textFive.substring(1, 13).toLowerCase() + " " + textFive.substring(textFive. indexOf(" ") + 1, textFive.indexOf(" ") + 2).toUpperCase() + textFive.substring(textFive.indexOf(" ") + 2 , textFive.length).toLowerCase();
+console.log(textCapitalized);
